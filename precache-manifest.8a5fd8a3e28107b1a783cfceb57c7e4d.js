2b8c640509dbc3892376bcaac1e7a6285fbb733f
Hansen <hansen0086@163.com>
2019-09-04 16:31:57 +1000

    Updates

---
+++ precache-manifest.8a5fd8a3e28107b1a783cfceb57c7e4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb2af770a55bd9701420fe576cea04c",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "0ff33d1c72e457475d02",
+    "url": "/ReactApp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5928327819fd5c7d6cf3",
+    "url": "/ReactApp/static/js/2.7c039cdd.chunk.js"
+  },
+  {
+    "revision": "0ff33d1c72e457475d02",
+    "url": "/ReactApp/static/js/main.0619c63f.chunk.js"
+  },
+  {
+    "revision": "cd9f7b55a251455b6fc4",
+    "url": "/ReactApp/static/js/runtime~main.d477d097.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
